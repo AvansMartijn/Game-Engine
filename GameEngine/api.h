198e37b6f3b21a55307d5a08f5897e53a3caecd3
Xandor Janssen <xpjh.janssen@student.avans.nl>
2020-09-29 10:56:51 +0200

    opzet class diagram

---
+++ GameEngine/api.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "FirstPartyFacade.h"	
+#include "ThirdPartyFacade.h"	
+class api
+{
+private:
+	FirstPartyFacade _firstPartyFacade;
+	ThirdPartyFacade _thirdPartyFacade;
+};
+
