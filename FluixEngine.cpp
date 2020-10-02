812bc54bd0b43f8d5425c8a11b84bd22f8f196f2
Xandor Janssen <xpjh.janssen@student.avans.nl>
2020-10-02 12:20:25 +0200

    Omgooien die handel

---
+++ FluixEngine.cpp
@@ -0,0 +1,1 @@
+#include "FluixEngine.h"
---
+++ FluixEngine.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ThirdPartyFacade.h"
+#include "GameObjectFacade.h"
+class FluixEngine
+{
+private:
+	ThirdPartyFacade _thirdPartyFacade;
+	GameObjectFacade _gameObjectFacade;
+public:
+	//alle onzin van first party
+};
+
