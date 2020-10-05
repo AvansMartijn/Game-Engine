b5c9658705f9018354014b80bc6b72ba34a948d2
Xandor Janssen <xpjh.janssen@student.avans.nl>
2020-10-05 10:09:34 +0200

    skeleton

--- CanWieldExtension.h
+++ CanWieldExtension.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "AbstractGameObjectExtension.h"
+#include "AbstractManageableItem.h"
+#include <vector>
 
 class CanWieldExtension : AbstractGameObjectExtension
 {
+private:
+	std::vector<AbstractManageableItem> items;
+
+public:
+	void addItem(AbstractManageableItem);
+	void removeItem(AbstractManageableItem);
 };
 
--- MoveExtension.h
+++ MoveExtension.h
@@ -3,6 +3,14 @@
 
 class MoveExtension : AbstractGameObjectExtension
 {
+private:
+	float  _velocityX;
+	float  _velocityY;
 
+	//voor een movable platform die heen hen weer gaat
+	float _startX;
+	float _startY;
+	float _endX;
+	float _endY;
 };
 
