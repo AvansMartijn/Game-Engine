62ce3d76ca00da39500a849e14c864827a782164
Xandor Janssen <xpjh.janssen@student.avans.nl>
2020-10-05 10:41:53 +0200

    Update GameObject.h

--- GameObject.h
+++ GameObject.h
@@ -8,5 +8,17 @@ class GameObject
 private:
 	std::vector<AbstractGameObjectExtension> _gameObjectExtensions;
 	PhysicalBody _physicalBody;
+
+	float _width;
+	float _height;
+
+	int _gridPositionX;
+	int _gridPositionY;
+
+public:
+	void addExtension();
+	bool hasExtension();
+	AbstractGameObjectExtension getExtension();
+
 };
 
