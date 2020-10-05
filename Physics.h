f8e29e2fb403269352302dcbd2e618ff2ebf9d6a
AvansChiel <cgmarts@avans.nl>
2020-10-05 13:02:34 +0200

    added change velocity, change position

--- Physics.h
+++ Physics.h
@@ -1,7 +1,14 @@
 #pragma once
+
+#include "GameObject.h"
+
+
 class Physics
 {
 private:
 	std::vector<GameObject> _gameObjects;
+public:
+	void changeVelocity(GameObject object, Vec2 velocity);
+	void changePosition(GameObject object, Vec2 position);
 };
 
