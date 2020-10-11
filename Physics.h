a0e698fc1ccad86bc6d5344edf87c3ad0063cfaa
AvansChiel <cgmarts@avans.nl>
2020-10-11 19:31:21 +0200

    physics docs

--- Physics.h
+++ Physics.h
@@ -23,12 +23,12 @@ public:
 	/// <param name="position">The new position</param>
 	void setPosition(shared_ptr<GameObject> object, Vec2 position);
 	/// <summary>
-	/// Update the position of a game object, applying all physics forces like gravity.
+	/// Update the position of a game object, applying it's velocity to it's position
 	/// </summary>
 	/// <param name="object">The object to be updated</param>
 	void updatePosition(shared_ptr<GameObject> object);
 	/// <summary>
-	/// Add gravity to a game object
+	/// Add the force of gravity to an objects velocity value.
 	/// </summary>
 	/// <param name="object">The game object to get Gravity</param>
 	void addGravityForce(shared_ptr<GameObject> object);
