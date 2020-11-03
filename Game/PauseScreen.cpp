116a693dc310d8e7d5ac055d78f224ba7fac9a73
Xandor Janssen <xpjh.janssen@student.avans.nl>
2020-11-03 14:23:36 +0100

    pause button

--- Game/PauseScreen.cpp
+++ Game/PauseScreen.cpp
@@ -17,12 +17,17 @@ void PauseScreen::onInit() {
 	resumeButton.onClick = [](AbstractGame* game) { game->switchScreen(Screens::MainGame); };
 	_uiElements.push_back(make_shared<ButtonUiElement>(resumeButton));
 
-	ButtonUiElement helpButton = ButtonUiElement("Help", { (1080 / 2) - 200, 300, 500, 100 }, bgColor, { 180, 102, 13 }, "Portal", 40);
+	ButtonUiElement helpButton = ButtonUiElement("Help", { (1080 / 2) - 200, 250, 500, 100 }, bgColor, { 180, 102, 13 }, "Portal", 40);
 	helpButton.registerGame(_game);
 	helpButton.onClick = [](AbstractGame* game) { game->switchScreen(Screens::Help); };
 	_uiElements.push_back(make_shared<ButtonUiElement>(helpButton));
 
-	ButtonUiElement quitGameButton= ButtonUiElement("Quit", { (1080 / 2) - 200, 500, 500, 100 }, bgColor, { 180, 102, 13 }, "Portal", 40);
+	ButtonUiElement RestartButton = ButtonUiElement("Restart", { (1080 / 2) - 200, 400, 500, 100 }, bgColor, { 180, 102, 13 }, "Portal", 40);
+	RestartButton.registerGame(_game);
+	RestartButton.onClick = [](AbstractGame* game) {  game->reset(); game->switchScreen(Screens::MainGame);};
+	_uiElements.push_back(make_shared<ButtonUiElement>(RestartButton));
+
+	ButtonUiElement quitGameButton= ButtonUiElement("Quit", { (1080 / 2) - 200, 550, 500, 100 }, bgColor, { 180, 102, 13 }, "Portal", 40);
 	quitGameButton.registerGame(_game);
 	quitGameButton.onClick = [](AbstractGame* game) { game->switchScreen(Screens::GameOver); };
 	_uiElements.push_back(make_shared<ButtonUiElement>(quitGameButton));
