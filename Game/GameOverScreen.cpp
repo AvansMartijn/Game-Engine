b142f832ba0ff2e160b9212bf7fd9340e1cd2d20
MikeDoge9 <mike_damen@home.nl>
2020-11-04 10:30:35 +0100

    clean up

--- Game/GameOverScreen.cpp
+++ Game/GameOverScreen.cpp
@@ -7,19 +7,20 @@ GameOverScreen::~GameOverScreen() {}
 
 void GameOverScreen::onInit()
 {
-	Color bgColor = { 66, 99, 116 };
+	const Color bgColor = { 66, 99, 116 };
+	const string font = "Portal";
 
 	ImageUiElement backgroundImg = ImageUiElement("Background", { 0 , 0, 1080, 720 });
 	_uiElements.push_back(make_shared<ImageUiElement>(backgroundImg));
 
-	TextUiElement headerText = TextUiElement("  GAME OVER   ", "Portal", 48, { 0, 0, 0, 0 }, { 32, 180, 226 }, { 7, 16, 29 }, true);
+	TextUiElement headerText = TextUiElement("  GAME OVER   ", font, 48, { 0, 0, 0, 0 }, { 32, 180, 226 }, { 7, 16, 29 }, true);
 	_uiElements.push_back(make_shared<TextUiElement>(headerText));
 
-	TextUiElement bodyText = TextUiElement("  Score: ", "Portal", 48, { 100, 100, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
+	TextUiElement bodyText = TextUiElement("  Score: ", font, 48, { 100, 100, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
 	_bodyText = make_shared<TextUiElement>(bodyText);
 	_uiElements.push_back(_bodyText);
 
-	ButtonUiElement quitGameButton = ButtonUiElement("Main menu", { (1080 / 2) - 200, 500, 500, 100 }, bgColor, { 180, 102, 13 }, "Portal", 40);
+	ButtonUiElement quitGameButton = ButtonUiElement("Main menu", { (1080 / 2) - 200, 500, 500, 100 }, bgColor, { 180, 102, 13 }, font, 40);
 	quitGameButton.registerGame(_game);
 	quitGameButton.onClick = [](AbstractGame* game) { game->switchScreen(Screens::MainMenu); };
 	_uiElements.push_back(make_shared<ButtonUiElement>(quitGameButton));
