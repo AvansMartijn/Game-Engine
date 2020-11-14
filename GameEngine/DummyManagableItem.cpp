c1dec53cbd23df8388c616a8b6a2d80c8d8a0ddb
AvansChiel <cgmarts@avans.nl>
2020-11-14 13:26:07 +0100

    calc angle from mouse to player

--- GameEngine/DummyManagableItem.cpp
+++ GameEngine/DummyManagableItem.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "DummyManagableItem.h"
+#include "Scene.h"
 #include <iostream>
 
 DummyManagableItem::DummyManagableItem() {
@@ -7,7 +8,11 @@ DummyManagableItem::DummyManagableItem() {
 }
 
 void DummyManagableItem::onLeftClick(int x, int y) {
-	std::cout << "x: " << x << " | " << "y: " << y << std::endl;
+	int playerX = (int)Scene::getInstance().getPlayer()->body.b2body->GetPosition().x;
+	int playerY = (int)Scene::getInstance().getPlayer()->body.b2body->GetPosition().y;
+	double angleDeg = atan2((x/40.0f) - playerX, (y/40.0f) - playerY) * (180.0f / 3.141592653589793238463f);
+
+	std::cout << "x: " << x << " | " << "y: " << y << " angle degrees: " << angleDeg << std::endl;
 }
 
 void DummyManagableItem::onRightClick(int x, int y) {
