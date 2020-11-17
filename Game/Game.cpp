244119c67cb01b85ad79fcc2a0a9172e81e0548d
Lars Kruijssen <44831215+LarsKruijssen2@users.noreply.github.com>
2020-11-17 11:18:41 +0100

    Mooi Main Menu Neef

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -23,7 +23,9 @@ void Game::onInit() {
 	registerTexture("Gate_Cropped", "res/gfx/Assets/Enviroment/Gate_Cropped.png");
 	registerTexture("Tile_Interior_Ground_Center", "res/gfx/Assets/Tiles/Interior/Ground_Center.png");
 	registerTexture("Border", "res/gfx/Border.png");
-	registerTexture("Background", "res/gfx/bg.png");
+	registerTexture("Background", "res/gfx/Background.png");
+	registerTexture("Logo", "res/gfx/LatropLogo.png");
+	registerTexture("LogoWesley", "res/gfx/LatropLogoWesley.png");
 	registerFont("OpenSans", "res/fonts/OpenSans-Regular.ttf");
 	registerFont("Paint", "res/fonts/Paint Drops.ttf");
 	registerFont("Portal", "res/fonts/PortalFont.ttf");
