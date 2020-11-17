efc71a7d49f08e21ca19f079b16f40eb9908ee2c
Lars Kruijssen <44831215+LarsKruijssen2@users.noreply.github.com>
2020-11-17 13:33:36 +0100

    :(

--- Game/GameOverScreen.cpp
+++ Game/GameOverScreen.cpp
@@ -14,29 +14,21 @@ void GameOverScreen::onInit()
 	ImageUiElement backgroundImg = ImageUiElement("Background", { 0 , 0, 1080, 720 });
 	_uiElements.push_back(make_shared<ImageUiElement>(backgroundImg));
 
-	TextUiElement headerText = TextUiElement("Game Over", font, 60, { 0, 0, 0, 0 }, { 255, 255, 255 }, bgColor, true);
+	TextUiElement headerText = TextUiElement("Game Over, you lost", font, 60, { 0, 0, 0, 0 }, { 255, 255, 255 }, bgColor, true);
 	_uiElements.push_back(make_shared<TextUiElement>(headerText));
 
-	TextUiElement bodyText = TextUiElement("Score: ", font, 40, { 100, 100, 0, 0 }, { 255, 255, 255 }, bgColor, true);
+	TextUiElement bodyText = TextUiElement("Score: 0", font, 40, { 100, 100, 0, 0 }, { 255, 255, 255 }, bgColor, true);
 	_bodyText = make_shared<TextUiElement>(bodyText);
 	_uiElements.push_back(_bodyText);
 
-	ButtonUiElement quitGameButton = ButtonUiElement("Main menu", { 500, 650, 150, 40 }, bgColor, { 255, 255, 255 }, font, 25);
-	TextUiElement headerText = TextUiElement("  GAME OVER: YOU LOSE  ", font, 48, { 0, 0, 0, 0 }, { 32, 180, 226 }, { 7, 16, 29 }, true);
-	_uiElements.push_back(make_shared<TextUiElement>(headerText));
-
-	TextUiElement bodyText = TextUiElement("  Score (DOESN'T COUNT): ", font, 48, { 100, 100, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
-	_bodyText = make_shared<TextUiElement>(bodyText);
-	_uiElements.push_back(_bodyText);
-
-	TextUiElement nameLabelText = TextUiElement("  Name:   ", font, 48, { 100, 200, 0, 0 }, { 32, 180, 226 }, { 7, 16, 29 }, true);
+	TextUiElement nameLabelText = TextUiElement("Name:", font, 40, { 100, 200, 0, 0 }, { 255, 255, 255 }, bgColor, true);
 	_uiElements.push_back(make_shared<TextUiElement>(nameLabelText));
 
-	TextUiElement nameText = TextUiElement(" ", font, 48, { 100, 300, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
+	TextUiElement nameText = TextUiElement(" ", font, 40, { 100, 300, 0, 0 }, { 255, 255, 255 }, bgColor, true);
 	_nameText = make_shared<TextUiElement>(nameText);
 	_uiElements.push_back(_nameText);
 
-	ButtonUiElement quitGameButton = ButtonUiElement("Main menu", { (1080 / 2) - 200, 500, 500, 100 }, bgColor, { 180, 102, 13 }, font, 40);
+	ButtonUiElement quitGameButton = ButtonUiElement("Main menu", { 500, 650, 200, 40 }, bgColor, { 255, 255, 255 }, font, 25);
 	quitGameButton.registerGame(_game);
 	quitGameButton.onClick = [](AbstractGame* game) { game->switchScreen(Screens::MainMenu); };
 	_uiElements.push_back(make_shared<ButtonUiElement>(quitGameButton));
@@ -46,7 +38,7 @@ void GameOverScreen::onTick() {}
 
 void GameOverScreen::onScreenShowed(vector<string> args) {
 
-	_bodyText->text = "  Score (DOESN'T COUNT): " + to_string(Scene::getInstance().score);
+	_bodyText->text = "  Score: " + to_string(Scene::getInstance().score);
 }
 
 void GameOverScreen::handleKeyboardInput(SDL_KeyboardEvent e) {
--- Game/HighscoreScreen.cpp
+++ Game/HighscoreScreen.cpp
@@ -18,46 +18,39 @@ void HighScoreScreen::onInit()
 	ImageUiElement backgroundImg = ImageUiElement("Background", { 0 , 0, 1080, 720 });
 	_uiElements.push_back(make_shared<ImageUiElement>(backgroundImg));
 
-	TextUiElement headerText = TextUiElement("Highscores", font, 60, { 10, 10, 0, 0 }, { 255, 255, 255 }, { 28, 28, 28 }, true);
+	TextUiElement headerText = TextUiElement("Highscores", font, 60, { 10, 10, 0, 0 }, { 255, 255, 255 }, bgColor, true);
 	_uiElements.push_back(make_shared<TextUiElement>(headerText));
 
-	TextUiElement bodyText = TextUiElement("Level 1", font, 25, { 100, 160, 0, 0 }, { 255, 255, 255 }, { 28, 28, 28 }, true);
-	_bodyText = make_shared<TextUiElement>(bodyText);
-	_uiElements.push_back(_bodyText);
+	TextUiElement _bodyText = TextUiElement("Level 1", font, 25, { 100, 160, 0, 0 }, { 255, 255, 255 }, bgColor, true);
+	_uiElements.push_back(make_shared<TextUiElement>(_bodyText));
 
 	ImageUiElement backgroundImg2 = ImageUiElement("Line", { 230 , 200, 620, 1});
 	_uiElements.push_back(make_shared<ImageUiElement>(backgroundImg2));
+
+	TextUiElement row1Text = TextUiElement(" - ", "Portal", 25, { 100, 250, 0, 0 }, { 255, 255, 255 }, bgColor, true);
+	_row1Text = make_shared<TextUiElement>(row1Text);
+	_uiElements.push_back(_row1Text);
+
+	TextUiElement row2Text = TextUiElement(" - ", "Portal", 25, { 100, 300, 0, 0 }, { 255, 255, 255 }, bgColor, true);
+	_row2Text = make_shared<TextUiElement>(row2Text);
+	_uiElements.push_back(_row2Text);
+
+	TextUiElement row3Text = TextUiElement(" - ", "Portal", 25, { 100, 350, 0, 0 }, { 255, 255, 255 }, bgColor, true);
+	_row3Text = make_shared<TextUiElement>(row3Text);
+	_uiElements.push_back(_row3Text);
+
+	TextUiElement row4Text = TextUiElement(" - ", "Portal", 25, { 100, 400, 0, 0 }, { 255, 255, 255 }, bgColor, true);
+	_row4Text = make_shared<TextUiElement>(row4Text);
+	_uiElements.push_back(_row4Text);
+
+	TextUiElement row5Text = TextUiElement(" - ", "Portal", 25, { 100, 450, 0, 0 }, { 255, 255, 255 }, bgColor, true);
+	_row5Text = make_shared<TextUiElement>(row5Text);
+	_uiElements.push_back(_row5Text);
 
 	ButtonUiElement backButton = ButtonUiElement("Back", { 515, 650, 70, 40 }, bgColor, { 255, 255, 255 }, font, 25);
 	backButton.registerGame(_game);
 	backButton.onClick = [](AbstractGame* game) { game->switchScreen(Screens::GoBack); };
 	_uiElements.push_back(make_shared<ButtonUiElement>(backButton));
-	TextUiElement row1Text = TextUiElement(" - ", "Portal", 48, { 100, 100, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
-	_row1Text = make_shared<TextUiElement>(row1Text);
-	_uiElements.push_back(_row1Text);
-
-	TextUiElement row2Text = TextUiElement(" - ", "Portal", 48, { 100, 200, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
-	_row2Text = make_shared<TextUiElement>(row2Text);
-	_uiElements.push_back(_row2Text);
-
-	TextUiElement row3Text = TextUiElement(" - ", "Portal", 48, { 100, 300, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
-	_row3Text = make_shared<TextUiElement>(row3Text);
-	_uiElements.push_back(_row3Text);
-
-	TextUiElement row4Text = TextUiElement(" - ", "Portal", 48, { 100, 400, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
-	_row4Text = make_shared<TextUiElement>(row4Text);
-	_uiElements.push_back(_row4Text);
-
-	TextUiElement row5Text = TextUiElement(" - ", "Portal", 48, { 100, 500, 0, 0 }, { 180, 102, 13 }, { 7, 16, 29 }, true);
-	_row5Text = make_shared<TextUiElement>(row5Text);
-	_uiElements.push_back(_row5Text);
-
-
-	ButtonUiElement quitGameButton = ButtonUiElement("Back", { (1080 / 2) - 200, 600, 500, 100 }, bgColor, { 180, 102, 13 }, font, 40);
-	quitGameButton.registerGame(_game);
-	quitGameButton.onClick = [](AbstractGame* game) { game->switchScreen(Screens::GoBack); };
-	_uiElements.push_back(make_shared<ButtonUiElement>(quitGameButton));
-
 }
 
 void HighScoreScreen::onTick() {}
