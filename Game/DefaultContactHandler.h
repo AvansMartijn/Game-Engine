71be66dca1dfb1847b07bbe1d0265eb28b479117
Wesley van Osch <whjposch@avans.nl>
2020-12-17 14:21:25 +0100

    Update DefaultContactHandler.h

--- Game/DefaultContactHandler.h
+++ Game/DefaultContactHandler.h
@@ -47,10 +47,48 @@ public:
 	/// <param name="valB">Second object.</param>
 	void checkExitSensor(const CustomUserData& valA, const CustomUserData& valB);
 
+	/// <summary>
+	/// Checks if the pickup sensor was triggered.
+	/// </summary>
+	/// <param name="valA"></param>
+	/// <param name="valB"></param>
+	/// <param name="gameObjectA"></param>
+	/// <param name="gameObjectB"></param>
 	void checkPickupSensor(const CustomUserData& valA, const CustomUserData& valB, GameObject* gameObjectA, GameObject* gameObjectB);
+
+	/// <summary>
+	/// Checks if the glue bullet sensor was triggered.
+	/// </summary>
+	/// <param name="valA"></param>
+	/// <param name="valB"></param>
+	/// <param name="gameObject"></param>
+	/// <param name="objA"></param>
+	/// <param name="fixtureA"></param>
 	void checkGlueBullet(CustomUserData& valA, const CustomUserData& valB, GameObject* gameObject, const CustomUserData& objA, const std::unique_ptr<Fixture>& fixtureA);
+
+	/// <summary>
+	/// Checks if the portal bullet sensor was triggered.
+	/// </summary>
+	/// <param name="valA"></param>
+	/// <param name="valB"></param>
+	/// <param name="objA"></param>
+	/// <param name="gameObjectA"></param>
+	/// <param name="gameObjectB"></param>
 	void checkPortalBullet(const CustomUserData& valA, const CustomUserData& valB, const CustomUserData& objA, GameObject* gameObjectA, GameObject* gameObjectB);
+
+	/// <summary>
+	/// Checks if the teleport sensor was triggered.
+	/// </summary>
+	/// <param name="gameObjectA"></param>
+	/// <param name="gameObjectB"></param>
+	/// <param name="valB"></param>
 	void checkTeleport(GameObject* gameObjectA, GameObject* gameObjectB, const CustomUserData& valB);
+
+	/// <summary>
+	/// Damages the given game object.
+	/// </summary>
+	/// <param name="gameObjectA"></param>
+	/// <param name="gameObjectB"></param>
 	void checkDamage(GameObject* gameObjectA, GameObject* gameObjectB);
 };
 
